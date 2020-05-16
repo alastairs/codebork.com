e8c497deef7aeadcc0eac5d4ec3f436d989ac378
Alastair Smith <github@alastairsmith.me.uk>
2020-05-16 13:42:01 +0100

    Update new-post for categories
    Also update the template to include the author name, and a tags structure.

--- new-post.sh
+++ new-post.sh
@@ -2,11 +2,16 @@
 DATE=`date +%s`
 EDITOR=${EDITOR:-`which vim`}
 title=${1:-`read -p "What is the title of the blog post? " title; echo $title`}
+category=${2:-`read -p "What category should the post be filed under? " category; echo $category`}
 file=_posts/$(date -j -f "%s" $DATE +"%Y-%m-%d")-${title// /-}\.md
 cat <<POST > $file 
 ---
 title: $title
+author: Alastair Smith
+category: $category
 created: $DATE
+tags:
+ -
 ---
 POST
 
