cde02ab14203fe0a8d1323e2ed01c549c7d5b25c
Alastair Smith <github@alastairsmith.me.uk>
2020-07-01 22:09:24 +0100

    Add git steps to new-post script
    Automate all the things :+1:

--- new-post.sh
+++ new-post.sh
@@ -4,7 +4,7 @@ EDITOR=${EDITOR:-`which vim`}
 title=${1:-`read -p "What is the title of the blog post? " title; echo $title`}
 category=${2:-`read -p "What category should the post be filed under? " category; echo $category`}
 file=_posts/$(date -j -f "%s" $DATE +"%Y-%m-%d")-${title// /-}\.md
-cat <<POST > $file 
+cat <<POST > $file
 ---
 title: $title
 author: Alastair Smith
@@ -14,6 +14,11 @@ tags:
  -
 ---
 POST
+
+git fetch --prune
+git checkout -b "draft/${title// /-}" origin/master
+git add -A "$file"
+git commit -m "Created new post"
 
 $EDITOR $file
 
